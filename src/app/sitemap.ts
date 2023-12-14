6e868d327e6bf4d785908f6e44759506d6c2e940
Djroton Noé Sossou <70499809+noahdjroton@users.noreply.github.com>
2023-12-14 17:11:33 +0100

    update robots.txt

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: "https://djroton.com",
+            url: "https://djroton.com/",
             lastModified: new Date(),
             changeFrequency: "yearly",
             priority: 1,
